29193dfed671785598381c83409e7c9763ccbc45
MMS <msobral@ifsc.edu.br>
2023-04-14 17:16:36 +0000

    compila o spec tftp2.prot, e gera cdddigo rust

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ mod msg;
 
 use msg::Codec;
 
+// Include the `items` module, which is generated from items.proto.
+// It is important to maintain the same structure as in the proto.
+pub mod tftp2 {
+  pub mod spec {
+      include!(concat!(env!("OUT_DIR"), "/tftp2.rs"));
+  }
+}
+
+use tftp2::spec;
+
 #[derive(Debug)]
 pub enum Status {
   OK,
